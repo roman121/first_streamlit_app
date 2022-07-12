6926dd606d8899439fa7b92eef2ff7262304f6a2
Roman Prajapati <rmnprjpt@gmail.com>
2022-07-12 12:03:09 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.header ('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text(' 🐔  Hard-Boiled Free-Range eggs')
-Streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
