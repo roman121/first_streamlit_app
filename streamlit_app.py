7921c5eaa0315837c4ed674ee37cf995e15102c6
Roman Prajapati <rmnprjpt@gmail.com>
2022-07-12 11:55:55 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.header ('Breakfast Menu')
 
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 
-streamlit.text('Kale,Spinach and Rocket Smoothie')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range eggs')
